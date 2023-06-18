232ef7c3e13c9066a0d8e4d4ed45c3bf651298d5
rzamarefat <rzamarefat@gmail.com>
2023-06-18 17:25:06 +0000

    developing seg dataset modules

--- src/dataset/SegDatasetMaker.py
+++ src/dataset/SegDatasetMaker.py
@@ -1,6 +1,17 @@
+import cv2
+import numpy as np
+
 class SegDatasetMaker:
     def __init__(self):
         pass
 
+    def rotate_image(image, angle):
+        image_center = tuple(np.array(image.shape[1::-1]) / 2)
+        rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
+        result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
+        return result
+
     def make(self):
         pass
+
+    
---
+++ src/dataset/config.py
@@ -0,0 +1,5 @@
+config = {
+    "root_to_save_watermarked_images": "",
+    "root_to_read_original_images": "",
+    "num_required_images": 10,
+}
