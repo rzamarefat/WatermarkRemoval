29b4ad9fb1e9d8a6d0fb141dded6d7dc0a938d82
rzamarefat <rzamarefat@gmail.com>
2023-06-19 07:28:18 +0000

    developing seg dataset

--- src/dataset/SegDatasetMaker.py
+++ src/dataset/SegDatasetMaker.py
@@ -9,6 +9,8 @@ class SegDatasetMaker:
     def __init__(self):
         self._chars = [c for c in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUWXYX1234567890!@#$%^&*()_+="]
         self._max_text_length = 15
+        self._aug_methods = ["rotate", "void", "sharpness"]
+        self._watermark_methods = ["tile", "single"]
 
         self._root_to_read_original_images = config["root_to_read_original_images"]
 
@@ -37,19 +39,44 @@ class SegDatasetMaker:
         image = cv2.imread(random.sample(self._bg_images_p, 1)[0])
         return image
 
+    def _make_mask_image(self, bg_image, watermark_method, texts):
+        black_image = np.zeros_like(bg_image)
+
+
+        if watermark_method == "tile":
+            mask_images = []
+            for text in texts:
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                org = (50, 50)
+                fontScale = 1
+                color = (255, 0, 0)
+                thickness = 2
+                maks_image = cv2.putText(black_image, text, org, font, fontScale, color, thickness, cv2.LINE_AA)
+                mask_images.append(mask_images)
+
+            return mask_images
+
+        elif watermark_method == "single":
+            return None 
+
+
     def _select_log(self):
         logo = None
         return logo
 
     def make(self):
-        toss_for = random.sample([f for f in range(10)], 1)[0]
-        toss_for_text_length = random.sample([f for f in range(self._max_text_length)], 1)[0]
-        
-        print(toss_for_text_length)
-        rand_text = self._generate_text(toss_for_text_length)
+        rand_watermark_method = random.sample([self._watermark_methods], 1)[0]
+        rand_text_length = random.sample([f for f in range(self._max_text_length)], 1)[0]
+        rand_text = self._generate_text(rand_text_length)
         rand_bg = self._select_random_bg()
+        
+        if config["do_augmentation_on_bgs"]:
+            toss_for_aug_method = random.sample([i for i in range(len(self._aug_methods))], 1)[0]
 
-        print(rand_bg.shape)
+
+        self._make_mask_image(rand_bg, rand_watermark_method, rand_text)
+            
+        
 
 
 if __name__ == "__main__":
--- src/dataset/config.py
+++ src/dataset/config.py
@@ -3,4 +3,5 @@ config = {
     # The following should be set to the dir which directly includes images - No subdir
     "root_to_read_original_images": "/home/rmarefat/projects/NSFW_zip_files_dataset/nudenet_predicted_gantman/training/neutral",
     "num_required_images": 10,
+    "do_augmentation_on_bgs": True
 }
